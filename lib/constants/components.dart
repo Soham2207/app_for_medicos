33fdbabe92f7c1e64a61decc28000677f55b3f24
Yashs1623 <yashs230602@gmail.com>
2022-01-17 19:29:58 +0530

    Optimised Code of Anantomy Section

---
+++ lib/constants/components.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const textStyle = TextStyle(
+  fontFamily: 'SansitaSwashed',
+  color: Color(0xff0cbaba),
+  fontSize: 30.0,
+  fontWeight: FontWeight.bold,
+);
